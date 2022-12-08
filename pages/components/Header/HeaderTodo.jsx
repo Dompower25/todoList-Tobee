07a51ecd6edb8d962b7eb2741a6c43193ad461bd
Eugene Domanovich <dompro1994.ed@gmail.com>
2022-12-08 21:35:38 +0300

    created components

---
+++ pages/components/Header/HeaderTodo.jsx
@@ -0,0 +1,9 @@
+import "./HeaderTodo.module.css"
+
+const HeaderTodo = () => {
+  return (
+    <div className="appName">todos</div>
+  )
+}
+
+export default HeaderTodo
---
+++ pages/components/LearnBar/LearnBar.jsx
@@ -0,0 +1,10 @@
+import "./LearnBar.module.css"
+import HeaderTodo from "../Header/HeaderTodo"
+
+const LearnBar = () => {
+  return (
+    <div className="learn-bar">      <HeaderTodo /></div>
+  )
+}
+
+export default LearnBar
